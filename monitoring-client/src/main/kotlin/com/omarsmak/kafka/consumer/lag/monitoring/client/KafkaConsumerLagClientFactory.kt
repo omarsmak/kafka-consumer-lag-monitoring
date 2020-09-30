e303b001ecd146fc959ee23035004088e4fc99b9
Omar Al-Safi <omarsmak@gmail.com>
2020-09-30 17:44:24 +0200

    Progress on multi project

--- monitoring-client/src/main/kotlin/com/omarsmak/kafka/consumer/lag/monitoring/client/KafkaConsumerLagClientFactory.kt
+++ monitoring-client/src/main/kotlin/com/omarsmak/kafka/consumer/lag/monitoring/client/KafkaConsumerLagClientFactory.kt
@@ -1,7 +1,6 @@
 package com.omarsmak.kafka.consumer.lag.monitoring.client
 
 import com.omarsmak.kafka.consumer.lag.monitoring.client.impl.KafkaConsumerLagJavaClient
-import com.omarsmak.kafka.consumer.lag.monitoring.config.KafkaConsumerLagClientConfig
 import java.util.Properties
 import org.apache.kafka.clients.admin.AdminClient
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -15,9 +14,6 @@ import org.apache.kafka.common.serialization.Serdes
 object KafkaConsumerLagClientFactory {
     @JvmStatic
     fun create(prop: Properties): KafkaConsumerLagClient = createJavaClient(prop)
-
-    @JvmStatic
-    fun create(config: KafkaConsumerLagClientConfig): KafkaConsumerLagClient = createJavaClient(config.toProperties())
 
     @JvmStatic
     fun create(map: Map<String, Any>): KafkaConsumerLagClient = createJavaClient(Properties().apply {
